8f33b58d53e98af32d2804cde08ece93800f9613
joana <joanamartasouza@gmail.com>
2025-04-17 17:03:09 +0400

    17/04/25

--- projeto_escola-master/src/main/java/com/senai/projeto_escola/service/ProfessorService.java
+++ projeto_escola-master/src/main/java/com/senai/projeto_escola/service/ProfessorService.java
@@ -3,25 +3,32 @@ package com.senai.projeto_escola.service;
 import com.senai.projeto_escola.application.dto.CoordenadorDto;
 import com.senai.projeto_escola.application.dto.ProfessorDto;
 import com.senai.projeto_escola.domain.entity.Professor;
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.List;
 
 @Service
 public class ProfessorService {
     @Autowired
-    private ProfessorService professorRepo;
+    private ProfessorService professorRpo;
 
     public void salvar(ProfessorDto professorDto){
         Professor professor = new Professor();
         professor.setNome(professorDto.nome());
         professor.setIdade(professorDto.idade());
         professor.setUnidadesCurriculares(professorDto.unidadesCurriculares());
-        professorRepo.salvar(professor);
+        professorRpo.salvar(professor);
     }
-    private List<ProfessorDto> listar(){
-        return professorRepo.findAll().stream().map(p-> new ProfessorDto
+    public List<CoordenadorDto> listar(){
+        return professorRpo.findAll().stream().map(p-> new ProfessorDto(
+                p.getId(),
+                p.getNome(),
+                p.getIdade(),
+                p.getUnidadesCurriculares()
+        )).toList();
     }
+    public Optional<ProfessorDto>buscarPorId(Long id)
+
 }
