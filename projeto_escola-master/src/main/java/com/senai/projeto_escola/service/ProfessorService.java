8eb336d352ed96d3974b1b2b62ea71fbfd5de212
joana <joanamartasouza@gmail.com>
2025-04-17 16:42:25 +0400

    17/04/25

--- projeto_escola-master/src/main/java/com/senai/projeto_escola/service/ProfessorService.java
+++ projeto_escola-master/src/main/java/com/senai/projeto_escola/service/ProfessorService.java
@@ -1,23 +1,27 @@
 package com.senai.projeto_escola.service;
 
+import com.senai.projeto_escola.application.dto.CoordenadorDto;
 import com.senai.projeto_escola.application.dto.ProfessorDto;
 import com.senai.projeto_escola.domain.entity.Professor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.util.List;
 
 @Service
 public class ProfessorService {
     @Autowired
-    private ProfessorService professorRpo;
+    private ProfessorService professorRepo;
 
     public void salvar(ProfessorDto professorDto){
         Professor professor = new Professor();
         professor.setNome(professorDto.nome());
         professor.setIdade(professorDto.idade());
-        professor.setUnidadesCurriculares(mapUnidadedCurriculares(professorDto.unidadesCurriculares()));
-        professorRpo.salvar(professor);
+        professor.setUnidadesCurriculares(professorDto.unidadesCurriculares());
+        professorRepo.salvar(professor);
     }
-    private List<Professor>
+    private List<ProfessorDto> listar(){
+        return professorRepo.findAll().stream().map(p-> new ProfessorDto
+    }
 }
